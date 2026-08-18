f78f585ad9b8c42ef02185230c83d6a2efc19136
emmatita <emma.bouttier@gmail.com>
2026-08-18 15:13:55 +0200

    Added a clear button and hid the original file input to turn the label into the button for style purposes

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const input = document.getElementById('pdfInput');
-const frame = document.getElementById('pdfFrame')
+const frame = document.getElementById('pdfFrame');
+const clearBtn = document.getElementById('clearBtn');
+const fileNameLabel = document.getElementById('fileName');
 
 input.addEventListener('change', (event) => {
     const file = event.target.files[0];
     if (!file) return;
 
+    fileNameLabel.textContent = file.name;
+
     const fileURL = URL.createObjectURL(file);
     frame.src = fileURL;
-    frame.style.display = 'block';});
+    frame.style.display = 'block';
+
+    clearBtn.style.display = 'inline-block'; });
+
+clearBtn.addEventListener('click', () => {
+    input.value = '';
+    frame.src = '';
+    frame.style.display = 'none';
+    fileNameLabel.textContent = 'No file chosen';
+    clearBtn.style.display = 'none'; });
